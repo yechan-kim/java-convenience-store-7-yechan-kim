2cd9c962589f19e0c18da801ad4de01ed980b0be
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 02:49:32 +0900

    feat(Product): 제품 모델 추가 및 데이터 변환 메서드 구현
    - `Product` 클래스 추가하여 제품 관련 정보 관리
    - `of` 메서드로 프로모션이 포함된 제품 객체 생성
    - `from` 메서드로 프로모션 없이 제품 객체 생성
    - 제품 이름, 가격, 수량, 프로모션 수량, 프로모션 정보를 속성으로 포함

---
+++ src/main/java/store/model/Product.java
@@ -0,0 +1,37 @@
+package store.model;
+
+import java.util.List;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private final Promotion promotion;
+
+    private final int promotionQuantity;
+    private int quantity;
+
+    private Product(String name, int price, int quantity, int promotionQuantity, Promotion promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotionQuantity = promotionQuantity;
+        this.promotion = promotion;
+    }
+
+    public static Product of(List<String> values, Promotion promotion) {
+        return new Product(values.get(0), Integer.parseInt(values.get(1)), 0, Integer.parseInt(values.get(2)),
+                promotion);
+    }
+
+    public static Product from(List<String> values) {
+        return new Product(values.get(0), Integer.parseInt(values.get(1)), Integer.parseInt(values.get(2)), 0, null);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void updateQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
