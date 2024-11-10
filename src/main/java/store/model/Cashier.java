c6a6865b2435ebaad37ad89230b142102de982c0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 03:52:47 +0900

    feat(Cashier): 장바구니 상품 결제 처리 및 예외 처리 구현
    - 장바구니 상품에 대한 결제 처리 로직 추가
    - 재고 부족 및 상품 미존재 시 예외 처리
    - 결제 성공 시 `PurchasedProduct` 반환

---
+++ src/main/java/store/model/Cashier.java
@@ -0,0 +1,43 @@
+package store.model;
+
+import java.util.List;
+import store.exception.ErrorMessage;
+import store.exception.StoreException;
+
+public class Cashier {
+
+    private final List<Product> productList;
+
+    private Cashier(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public static Cashier from(List<Product> productList) {
+        return new Cashier(productList);
+    }
+
+    public List<PurchasedProduct> requestPayment(List<ShoppingCartProduct> shoppingCart) {
+        return shoppingCart.stream()
+                .map(this::processShoppingCartProduct)
+                .toList();
+    }
+
+    private PurchasedProduct processShoppingCartProduct(ShoppingCartProduct shoppingCartProduct) {
+        Product product = findProduct(productList, shoppingCartProduct.getName());
+        int quantity = shoppingCartProduct.getQuantity();
+
+        if (product.getPromotionQuantity() + product.getQuantity() < quantity) {
+            throw StoreException.from(ErrorMessage.STOCK_EXCEEDED);
+        }
+
+        return PurchasedProduct.of(product.getName(), product.getPrice(), product.getPromotionQuantity(), quantity,
+                product.getPromotion());
+    }
+
+    private Product findProduct(List<Product> productList, String name) {
+        return productList.stream()
+                .filter(product -> product.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> StoreException.from(ErrorMessage.NON_EXISTENT_PRODUCT));
+    }
+}
