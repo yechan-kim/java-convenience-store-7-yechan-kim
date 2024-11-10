55c5656451e698f393550b80f395be4d73674c59
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 04:15:05 +0900

    feat(OutputView): 새로운 메시지 형식 추가
    - 프로모션이 적용되지 않은 제품에 대한 메시지(`NO_PROMOTION_FORMAT`) 추가
    - 무료 프로모션 제품에 대한 메시지(`FREE_PRODUCT_PROMOTION_MESSAGE`) 추가
    - 사용자에게 추가적인 명령 입력을 유도하는 메시지 형식(`Y`/`N`) 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -6,9 +6,12 @@ import store.model.Product;
 public class OutputView {
 
     private static final String BUY_PRODUCT_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String COMMAND_SUFFIX = " (Y/N)";
     private static final String CURRENT_PRODUCTS_MESSAGE = "현재 보유하고 있는 상품입니다.";
+    private static final String FREE_PRODUCT_PROMOTION_MESSAGE = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까?";
     private static final String GREETING_MESSAGE = "안녕하세요. W편의점입니다.";
     private static final String NON_PROMOTION_PRODUCT_FORMAT = "- %s %,d원 %s";
+    private static final String NO_PROMOTION_FORMAT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까?";
     private static final String OUT_OF_STOCK = "재고 없음";
     private static final String PROMOTION_PRODUCT_FORMAT = "- %s %,d원 %s %s";
     private static final String QUANTITY_SUFFIX = "개";
@@ -25,6 +28,17 @@ public class OutputView {
     public void buyProductMessage() {
         System.out.println(System.lineSeparator() + BUY_PRODUCT_MESSAGE);
     }
+
+    public void noPromotionMessage(String name, int quantity) {
+        System.out.printf(System.lineSeparator() + NO_PROMOTION_FORMAT, name, quantity);
+        System.out.println(COMMAND_SUFFIX);
+    }
+
+    public void freeProductPromotionMessage(String name) {
+        System.out.printf(FREE_PRODUCT_PROMOTION_MESSAGE, name);
+        System.out.println(COMMAND_SUFFIX);
+    }
+
 
     public void products(List<Product> products) {
         for (Product product : products) {
