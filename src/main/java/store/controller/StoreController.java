cb2666709b63599ac3324f1f01fcbd61d0b8b5ff
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 05:04:57 +0900

    feat(StoreController): 결제 후 재고 업데이트 및 영수증 출력 로직 추가
    - `Receipt` 클래스를 추가하여 영수증 출력 기능을 담당하도록 변경
    - `StockUpdater` 클래스를 사용하여 결제 후 재고를 업데이트하는 기능 추가
    - `processReceipt` 메서드에서 영수증 출력과 멤버십 할인 적용 처리
    - `processPayment` 메서드에서 결제 처리 후 영수증 출력과 재고 업데이트 호출

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -5,14 +5,18 @@ import store.model.Cashier;
 import store.model.MemberShipDiscountHandler;
 import store.model.Product;
 import store.model.PurchasedProduct;
+import store.model.Receipt;
 import store.model.ShoppingCartProduct;
+import store.model.StockUpdater;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class StoreController {
 
+    private final Receipt receipt = new Receipt();
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
+    private final StockUpdater stockUpdater = new StockUpdater();
     private final MemberShipDiscountHandler memberShipDiscountHandler = new MemberShipDiscountHandler();
     private final List<Product> productList = inputView.readProductList();
 
@@ -38,11 +42,19 @@ public class StoreController {
         List<ShoppingCartProduct> shoppingCart = getShoppingCart();
         Cashier cashier = Cashier.from(productList);
         List<PurchasedProduct> purchasedProductList = cashier.requestPayment(shoppingCart);
-        int memberShipDiscount = memberShipDiscountHandler.apply(purchasedProductList);
+
+        processReceipt(purchasedProductList);
+        stockUpdater.updateStock(purchasedProductList, productList);
     }
 
     private List<ShoppingCartProduct> getShoppingCart() {
         outputView.buyProductMessage();
         return ShoppingCartProduct.createList(inputView.readShoppingCart().rawShoppingCart());
+    }
+
+    private void processReceipt(List<PurchasedProduct> purchasedProductList) {
+        int memberShipDiscount = memberShipDiscountHandler.apply(purchasedProductList);
+
+        receipt.printReceipt(purchasedProductList, memberShipDiscount);
     }
 }
