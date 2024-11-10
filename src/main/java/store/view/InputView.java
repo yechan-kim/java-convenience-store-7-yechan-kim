2b08ffff97aaba1f1987d31f7d2d62887eb70350
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 03:37:26 +0900

    feat(InputView): 장바구니 입력 및 형식 검증 기능 추가
    - 사용자가 입력한 장바구니 형식을 검증하고, 올바른 형식일 경우 객체로 변환하여 반환
    - 잘못된 형식의 입력 시 재입력을 유도하는 예외 처리 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,18 +1,22 @@
 package store.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import store.dto.ShoppingCartInput;
 import store.model.Product;
 import store.model.Promotion;
 import store.util.FileLoader;
 import store.util.ProductPromotionParser;
+import store.validator.InputValidator;
 
 public class InputView {
 
     private static final String PRODUCT_FILE_PATH = "src/main/resources/products.md";
     private static final String PROMOTION_FILE_PATH = "src/main/resources/promotions.md";
 
-    private final ProductPromotionParser parser = new ProductPromotionParser();
     private final FileLoader fileLoader = new FileLoader();
+    private final InputValidator validator = new InputValidator();
+    private final ProductPromotionParser parser = new ProductPromotionParser();
 
     public List<Product> readProductList() {
         return parser.parseProducts(fileLoader.load(PRODUCT_FILE_PATH), readPromotionList());
@@ -20,5 +24,18 @@ public class InputView {
 
     private List<Promotion> readPromotionList() {
         return parser.parsePromotions(fileLoader.load(PROMOTION_FILE_PATH));
+    }
+
+    public ShoppingCartInput readShoppingCart() {
+        while (true) {
+            try {
+                String rawShoppingCart = Console.readLine().trim();
+                validator.shoppingCartFormat(rawShoppingCart);
+
+                return ShoppingCartInput.from(rawShoppingCart);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
