17d6558520221ebc27cf847f410dd9733f997fb3
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 03:13:56 +0900

    feat(OutputView): 상품 출력 기능 추가
    - 일반 상품과 프로모션 상품 구분 출력
    - 재고 없는 상품에 대해 "재고 없음" 메시지 출력
    - 프로모션 상품 정보 출력
    - 예외 메시지 출력 및 인트로 메시지 기능 추가

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,56 @@
+package store.view;
+
+import java.util.List;
+import store.model.Product;
+
+public class OutputView {
+
+    private static final String CURRENT_PRODUCTS_MESSAGE = "현재 보유하고 있는 상품입니다.";
+    private static final String GREETING_MESSAGE = "안녕하세요. W편의점입니다.";
+    private static final String NON_PROMOTION_PRODUCT_FORMAT = "- %s %,d원 %s";
+    private static final String OUT_OF_STOCK = "재고 없음";
+    private static final String PROMOTION_PRODUCT_FORMAT = "- %s %,d원 %s %s";
+    private static final String QUANTITY_SUFFIX = "개";
+
+    public void errorMessage(Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+    public void intro() {
+        System.out.println(GREETING_MESSAGE);
+        System.out.println(CURRENT_PRODUCTS_MESSAGE + System.lineSeparator());
+    }
+
+    public void products(List<Product> products) {
+        for (Product product : products) {
+            if (product.getPromotion() != null) {
+                promotionProduct(product);
+            }
+
+            nonPromotionProduct(product);
+        }
+    }
+
+    private void nonPromotionProduct(Product product) {
+        if (product.getQuantity() == 0) {
+            System.out.printf(NON_PROMOTION_PRODUCT_FORMAT, product.getName(), product.getPrice(),
+                    OUT_OF_STOCK + System.lineSeparator());
+            return;
+        }
+
+        System.out.printf(NON_PROMOTION_PRODUCT_FORMAT, product.getName(), product.getPrice(),
+                product.getQuantity() + QUANTITY_SUFFIX + System.lineSeparator());
+    }
+
+    private void promotionProduct(Product product) {
+        if (product.getPromotionQuantity() == 0) {
+            System.out.printf(PROMOTION_PRODUCT_FORMAT, product.getName(), product.getPrice(), OUT_OF_STOCK,
+                    product.getPromotion().getName() + System.lineSeparator());
+            return;
+        }
+
+        System.out.printf(PROMOTION_PRODUCT_FORMAT, product.getName(), product.getPrice(),
+                product.getPromotionQuantity() + QUANTITY_SUFFIX,
+                product.getPromotion().getName() + System.lineSeparator());
+    }
+}
