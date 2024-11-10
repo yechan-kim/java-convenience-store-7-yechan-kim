3a91c0c64e3f07cfbaac87041f26d92711fd7345
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 03:30:58 +0900

    feat(ErrorMessage): 새로운 에러 메시지 추가
    - 입력 형식이 올바르지 않은 경우의 에러 메시지 "올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요." 추가

--- src/main/java/store/exception/ErrorMessage.java
+++ src/main/java/store/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package store.exception;
 
 public enum ErrorMessage {
 
-    FILE_LOAD_ERROR("파일을 불러오는데 실패했습니다.");
+    FILE_LOAD_ERROR("파일을 불러오는데 실패했습니다."),
+    INVALID_FORMAT("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
 
     private final String message;
 
