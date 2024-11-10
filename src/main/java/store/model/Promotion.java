8a043fef94edd7fdbb1564482ba3fd35d134fd26
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 02:48:44 +0900

    feat(Promotion): 프로모션 모델 추가 및 데이터 변환 메서드 구현
    - `Promotion` 클래스 추가하여 프로모션 관련 정보 관리
    - `from` 메서드를 통해 리스트로부터 프로모션 객체를 생성하는 기능 구현
    - 프로모션 이름, 구매 수량, 제공 수량, 시작일, 종료일을 속성으로 포함

---
+++ src/main/java/store/model/Promotion.java
@@ -0,0 +1,29 @@
+package store.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    private Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static Promotion from(List<String> values) {
+        return new Promotion(values.get(0), Integer.parseInt(values.get(1)), Integer.parseInt(values.get(2)),
+                LocalDate.parse(values.get(3)), LocalDate.parse(values.get(4)));
+    }
+
+    public String getName() {
+        return name;
+    }
+}
