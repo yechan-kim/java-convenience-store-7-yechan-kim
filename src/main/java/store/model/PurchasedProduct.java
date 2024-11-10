90cf734ca142fdfe549c2f29c572ab3cfeeea2bd
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 03:47:44 +0900

    feat(PurchasedProduct): 구매한 상품 정보를 나타내는 PurchasedProduct 클래스 추가
    - 상품명, 가격, 수량, 프로모션 수량, 프로모션 정보를 저장하는 클래스 구현
    - `of` 팩토리 메서드로 객체 생성

---
+++ src/main/java/store/model/PurchasedProduct.java
@@ -0,0 +1,26 @@
+package store.model;
+
+public class PurchasedProduct {
+    private final String name;
+    private final int price;
+    private final int promotionQuantity;
+    private final int quantity;
+    private final Promotion promotion;
+
+    private PurchasedProduct(String name, int price, int promotionQuantity, int quantity, Promotion promotion) {
+        this.name = name;
+        this.price = price;
+        this.promotionQuantity = promotionQuantity;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public static PurchasedProduct of(String name, int price, int promotionQuantity, int quantity,
+                                      Promotion promotion) {
+        return new PurchasedProduct(name, price, promotionQuantity, quantity, promotion);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
