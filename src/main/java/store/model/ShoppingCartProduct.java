53b9e5573d4eaf8f2d0066b334103c3cb7187828
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 03:35:32 +0900

    feat(ShoppingCartProduct): 쇼핑카트 상품 생성 기능 구현
    - 문자열 형식으로부터 상품과 수량을 추출하여 객체로 생성하는 메서드 구현
    - 여러 상품을 한 번에 처리할 수 있는 기능 추가

---
+++ src/main/java/store/model/ShoppingCartProduct.java
@@ -0,0 +1,34 @@
+package store.model;
+
+import java.util.List;
+
+public class ShoppingCartProduct {
+
+    private final String name;
+    private final int quantity;
+
+    private ShoppingCartProduct(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public static ShoppingCartProduct of(String rawShoppingCartProduct) {
+        List<String> rawShoppingCartProductList = List.of(rawShoppingCartProduct.split("-"));
+
+        return new ShoppingCartProduct(rawShoppingCartProductList.get(0).substring(1),
+                Integer.parseInt(rawShoppingCartProductList.get(1)
+                        .substring(0, rawShoppingCartProductList.get(1).length() - 1)));
+    }
+
+    public static List<ShoppingCartProduct> createList(String rawShoppingCart) {
+        List<String> rawShoppingCartList = List.of(rawShoppingCart.split(","));
+
+        return rawShoppingCartList.stream()
+                .map(ShoppingCartProduct::of)
+                .toList();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
