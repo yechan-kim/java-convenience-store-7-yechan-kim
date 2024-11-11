f01678730a9feaf565d03f9027a118581873da1d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 14:15:23 +0900

    refactor(MemberShipDiscountHandler): 외부 입력 의존성 제거 및 명령어 파라미터화
    - `InputView와` `OutputView` 의존성을 제거하여 테스트 가능성을 개선
    - 명령어를 파라미터로 받아 외부에서 할인 적용 여부를 결정할 수 있도록 수정

--- src/main/java/store/model/MemberShipDiscountHandler.java
+++ src/main/java/store/model/MemberShipDiscountHandler.java
@@ -2,20 +2,14 @@ package store.model;
 
 import java.util.List;
 import store.controller.Command;
-import store.view.InputView;
-import store.view.OutputView;
 
 public class MemberShipDiscountHandler {
 
     private static final double DISCOUNT_RATE = 0.3;
     private static final int MAX_DISCOUNT = 8000;
 
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
-
-    public int apply(List<PurchasedProduct> purchasedProductList) {
-        outputView.membershipDiscountMessage();
-        if (inputView.readCommand().equals(Command.YES.getValue())) {
+    public int apply(List<PurchasedProduct> purchasedProductList, String command) {
+        if (command.equals(Command.YES.getValue())) {
             return calculateDiscount(purchasedProductList);
         }
 
