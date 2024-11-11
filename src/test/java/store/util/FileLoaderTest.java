a0c301a2e48bcba84242e0228c9e8a0aef887a50
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 14:23:16 +0900

    test(FileLoaderTest): 예외 타입 변경
    - `StoreException`에서 `IllegalArgumentException`으로 예외 타입 변경

--- src/test/java/store/util/FileLoaderTest.java
+++ src/test/java/store/util/FileLoaderTest.java
@@ -3,7 +3,6 @@ package store.util;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import store.exception.StoreException;
 
 public class FileLoaderTest {
 
@@ -11,6 +10,6 @@ public class FileLoaderTest {
     void 파일_읽기를_실패_하면_예외_발생() {
         String path = "test";
         FileLoader fileLoader = new FileLoader();
-        assertThrows(StoreException.class, () -> fileLoader.load(path));
+        assertThrows(IllegalArgumentException.class, () -> fileLoader.load(path));
     }
 }
