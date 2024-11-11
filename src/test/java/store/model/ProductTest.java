de70650b32f46995d04b162b6f73aa35554f8281
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 14:24:43 +0900

    test(ProductTest): 상품 관련 예외 처리 테스트 리팩토링
    - `assertSimpleTest.run()`에서 `InputValidator`와 `Cashier` 객체를 사용한 예외 처리 테스트로 변경
    - `IllegalArgumentException`을 사용해 예외를 명시적으로 검증
    - `ErrorMessage` `enum`을 활용하여 일관된 에러 메시지 처리
    - 테스트 코드의 가독성과 유지보수성 향상

--- src/test/java/store/model/ProductTest.java
+++ src/test/java/store/model/ProductTest.java
@@ -1,36 +1,46 @@
 package store.model;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import store.Application;
+import store.exception.ErrorMessage;
+import store.validator.InputValidator;
 
 public class ProductTest extends NsTest {
 
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    Cashier cashier = Cashier.from(List.of(Product.from(List.of("콜라", "1000", "10", "null"))));
+
     @Test
     void 구매할_상품과_수량_형식이_올바르지_않은_경우_예외_발생() {
-        assertSimpleTest(() -> {
-            run("콜라-3", "[콜라-3]", "N", "N");
-            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
-        });
+        String command = "콜라-3";
+        InputValidator validator = new InputValidator();
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> validator.shoppingCartFormat(command));
+        assertThat(exception.getMessage()).isEqualTo(ERROR_PREFIX + ErrorMessage.INVALID_FORMAT.getMessage());
     }
 
     @Test
     void 존재하지_않는_상품을_입력한_경우_예외_발생() {
-        assertSimpleTest(() -> {
-            run("[몬스터-3]", "[콜라-3]", "N", "N");
-            assertThat(output()).contains("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
-        });
+        List<ShoppingCartProduct> shoppingCart = List.of(ShoppingCartProduct.of("[몬스터-3]"));
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> cashier.requestPayment(shoppingCart));
+        assertThat(exception.getMessage()).isEqualTo(ERROR_PREFIX + ErrorMessage.NON_EXISTENT_PRODUCT.getMessage());
     }
 
     @Test
     void 재고가_없는_상품을_구매하려고_하는_경우_예외_발생() {
-        assertSimpleTest(() -> {
-            run("[오렌지주스-10]", "[오렌지주스-2]", "N", "N");
-            assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
-        });
+        List<ShoppingCartProduct> shoppingCart = List.of(ShoppingCartProduct.of("[콜라-11]"));
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> cashier.requestPayment(shoppingCart));
+        assertThat(exception.getMessage()).isEqualTo(ERROR_PREFIX + ErrorMessage.STOCK_EXCEEDED.getMessage());
     }
 
     @Override
