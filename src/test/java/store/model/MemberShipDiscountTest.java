1aca03b76b8ec031c103433f31ed3e55f5c73aae
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-11 14:23:45 +0900

    test(MemberShipDiscountTest): 테스트 코드 리팩토링
    - `assertSimpleTest.run()`에서 직접 `MemberShipDiscountHandler`를 사용하여 할인 적용 로직 테스트로 변경
    - `List<PurchasedProduct>`를 사용해 상품과 가격을 명확하게 표현
    - 테스트 코드의 간결성 및 가독성 향상

--- src/test/java/store/model/MemberShipDiscountTest.java
+++ src/test/java/store/model/MemberShipDiscountTest.java
@@ -1,36 +1,32 @@
 package store.model;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import store.Application;
 
 public class MemberShipDiscountTest extends NsTest {
 
+    private final MemberShipDiscountHandler memberShipDiscountHandler = new MemberShipDiscountHandler();
+
     @Test
     void 멤버십_할인_적용() {
-        assertSimpleTest(() -> {
-            run("[콜라-3],[에너지바-5]", "Y", "N");
-            assertThat(output().replaceAll("\\s", "")).contains("내실돈9,000");
-        });
+        List<PurchasedProduct> purchasedProducts = List.of(PurchasedProduct.of("콜라", 1000, 0, 1, null));
+        assertThat(memberShipDiscountHandler.apply(purchasedProducts, "Y")).isEqualTo(300);
     }
 
     @Test
     void 멤버십_할인_미적용() {
-        assertSimpleTest(() -> {
-            run("[콜라-3],[에너지바-5]", "N", "N");
-            assertThat(output().replaceAll("\\s", "")).contains("내실돈12,000");
-        });
+        List<PurchasedProduct> purchasedProducts = List.of(PurchasedProduct.of("콜라", 1000, 0, 1, null));
+        assertThat(memberShipDiscountHandler.apply(purchasedProducts, "N")).isEqualTo(0);
     }
 
     @Test
     void 멤버십_할인_한도_초과() {
-        assertSimpleTest(() -> {
-            run("[정식도시락-8]", "Y", "N");
-            assertThat(output().replaceAll("\\s", "")).contains("내실돈43,200");
-        });
+        List<PurchasedProduct> purchasedProducts = List.of(PurchasedProduct.of("정식도시락", 6400, 0, 8, null));
+        assertThat(memberShipDiscountHandler.apply(purchasedProducts, "Y")).isEqualTo(8000);
     }
 
     @Override
